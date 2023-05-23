a504ba58c683992d62766aa3eb55427c92b0160b
Marinos <marinostbh@gmail.com>
2023-05-23 23:05:37 +0100

    feat(on_ubuntu_linux)_added libraries related to linux

--- HealthReminder_modifcation.py
+++ HealthReminder_modifcation.py
@@ -1,11 +1,23 @@
-import winsound
+import os
 import time
+import logging
 from tkinter import *
 from tkinter import ttk
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+# Create a stream handler
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+# Add both handlers to the logger
+logger = logging.getLogger('')
+logger.addHandler(stream_handler)
 
 class HealthReminder(Tk):
-
     def __init__(self):
         super().__init__()
 
@@ -31,28 +43,49 @@ class HealthReminder(Tk):
         self.__center__ = f"{self.__width_app__}x{self.__height_app__}+{self.central_l_window}+{self.central_a_window}"
 
         self.geometry(self.__center__)
-        #Label
-        self.label = ttk.Label(self, text="Relax your eyes by look far(2,5meters Minimum)", font=("Arial", 18))
-
+        # Label
+        self.label = ttk.Label(self, text="Relax your eyes by looking far (2.5 meters minimum)", font=("Arial", 18))
         self.label.pack()
         self.label = ttk.Label(self, text="Move around for 120 seconds!", font=("Arial", 18))
         self.label.pack()
         self.label = ttk.Label(self, text="Drink a cup of water to stay hydrated!", font=("Arial", 18))
         self.label.pack()
-        #Button
+        # Button
         self.button = ttk.Button(self, text='Got it!')
         self.button['command'] = self.button_clicked
         self.button.pack()
 
+        # Start the application log
+        logging.info('Application started.')
 
     def button_clicked(self):
-        HealthReminder.destroy(self) #works with self
-beep_time = 60*30
+        # Stop the application log
+        logging.info('Application stopped.')
+
+        HealthReminder.destroy(self)
+
 def beep_every60():
+    beep_time = 60 * 30 
 
     while True:
-        winsound.Beep(1000,500)
-        HealthReminder().mainloop()
+        # Play system sound
+        os.system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga &")
+
+        # Create and run HealthReminder instance
+        reminder = HealthReminder()
+        reminder.mainloop()
+
         time.sleep(beep_time)
 
-beep_every60()
+if __name__ == '__main__':
+    # Start the main log
+    logging.info('Script execution started.')
+
+    # Run the application
+    try:
+        beep_every60()
+    except Exception as e:
+        logging.exception('An error occurred: %s', str(e))
+
+    # Stop the main log
+    logging.info('Script execution stopped.')
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-ECHO is on.
+This script is a standalone script that runs in python environment to remind you of getting breaks every 30 minutes following the Pomodoro working technique(25minutes work 5 minutes relax).
+
+
+Follow these steps: 
+
+clone repo :
+- git clone [git@github.com:MarinosTBH/python-scripts.git](git@github.com:MarinosTBH/python-scripts.git).
+- install tk gui(tkinter) :
+- sudo apt-get install python3-tk .
+- Make an executable with pyinstaller (if pyinstaller is not installed follow these steps):
+- pip install pyinstaller .
+
+**The executable will be located in dist dir called : /dist/HealthReminder_modifcation** 
+
+Now add the executable to the startup application on ubuntu:
+- Open the Startup Applications Preferences window by pressing Alt+F2 or ALT + Fn + F2 to open the "Run a Command" dialog, then type gnome-session-properties and press Enter.
+
+- In the Startup Applications Preferences window, click on the "Add" button to add a new startup application.
+
+- In the "Add Startup Program" dialog, provide the following information:
+
+    Name: Enter a descriptive name for your startup application, e.g., "Health Reminder".
+    Command: Enter the full path to your bash script, e.g., /path/to/health_reminder.sh.
+    Comment: Optionally, you can provide a brief description of your startup application.
+
+Click on the "Add" or "Add Program" button to add your startup application.
+
+Reboot the computer and it will be automatically compiling the script every 60 minutes to remind you.
